d19b76ff752ab76d5df42cf27057ca89b9c617a4
Muhammad junaid <161128073+Fsmjb@users.noreply.github.com>
2024-05-13 08:16:02 +0500

    Update binery.cpp
    update the code

--- binery.cpp
+++ binery.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 int main(){
    int  ans[1000];
-   int x = 50000, y = 1 ;
+   int x, y = 1 ;
    
-   while(x>= 2){
-    ans[y] = x % 2;
-    x /=2;
-    ++y;
+   cout << "Enter the number you want to conver : ";
+   cin >> x;
+   if (x >= 1){
+    while(x>= 2){
+        ans[y] = x % 2;
+        x /=2;
+        ++y;
+     }
+    ans[y]= 1;
+        while(y>=1){
+	    cout << ans[y];
+        --y;
+	    }
    }
-   ans[y]= 1;
-    while(y>=1){
-	cout << ans[y];
-    --y;
-	}
-	
 	return 0;
 }
