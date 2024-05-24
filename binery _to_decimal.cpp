c924f1c53d09821eaaa4099642593d83e606c71e
Muhammad junaid <161128073+Fsmjb@users.noreply.github.com>
2024-05-24 08:18:13 +0500

    Binary to Decimal converter
    This is simple c++ program which convert the binary to   decimal .

---
+++ binery _to_decimal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+char ch[100];
+int ans = 0;
+
+cout << "Enter the   binary number " ;
+cin >> ch;
+
+  for (int a = 0; a < 100; ++a) {
+       if (ch[a] == '\0') {
+           break;
+           } 
+       if ( ch [a] != '0' && ch[a] != '1'){
+           cout << "Invalid binary input. Please enter a valid binary number." << endl;
+         return 1;
+       }
+    
+       if (ch[a] == '1'){ 
+           ans += pow (2,a);
+       }
+    }
+cout <<"Decimal value : " << ans << endl;
+
+return 0;
+}
