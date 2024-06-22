43a99a37508c57115ff963957248c192e2a8edf5
Muhammad junaid <161128073+Fsmjb@users.noreply.github.com>
2024-06-22 04:34:16 +0000

    Update binery _to_decimal.cpp
    remove error from code

--- binery _to_decimal.cpp
+++ binery _to_decimal.cpp
@@ -5,23 +5,29 @@ using namespace std;
 
 int main(){
 char ch[100];
-int ans = 0;
+int ans = 0, num;
 
 cout << "Enter the   binary number " ;
 cin >> ch;
 
   for (int a = 0; a < 100; ++a) {
        if (ch[a] == '\0') {
+            num = a;
            break;
            } 
+
        if ( ch [a] != '0' && ch[a] != '1'){
            cout << "Invalid binary input. Please enter a valid binary number." << endl;
          return 1;
        }
-    
-       if (ch[a] == '1'){ 
-           ans += pow (2,a);
+    }
+    int b = 0;
+    num -= 1;
+    for (num; num >= 0; --num){
+       if (ch[num] == '1'){ 
+           ans += pow (2,b);
        }
+       ++b;
     }
 cout <<"Decimal value : " << ans << endl;
 
